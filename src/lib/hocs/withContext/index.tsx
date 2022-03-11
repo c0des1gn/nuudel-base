10ab980fd27c7f4a4609cef6837d3e78f677e934
Damdinsuren Dorjsuren <damdinsuren@live.com>
2022-03-11 18:47:30 +0800

    context

--- src/lib/hocs/withContext/index.tsx
+++ src/lib/hocs/withContext/index.tsx
@@ -1,9 +1,4 @@
-import React, {
-  useReducer,
-  createContext,
-  ComponentType,
-  useContext,
-} from 'react';
+import React, { useReducer, createContext, ComponentType } from 'react';
 import DeviceInfo from 'react-native-device-info';
 import { ViewMode } from 'nuudel-utils';
 
@@ -19,51 +14,59 @@ export const Context = createContext(initialState);
 Context.displayName = 'mainContext';
 
 const reducer = (state, action): any => {
-  if (action.type === 'assign') {
-    state = {
-      ...state,
-      ...action.value,
-    };
-  } else if (action.type === 'set' && action.prop) {
-    if (action.key) {
+  if (action.type === 'set' && action.payload?.prop) {
+    if (action.payload.key) {
       state = {
         ...state,
-        [action.prop]: { ...state[action.prop], [action.key]: action.value },
+        [action.payload.prop]: {
+          ...state[action.payload.prop],
+          [action.payload.key]: action.payload.value,
+        },
       };
     } else {
       state = {
         ...state,
-        [action.prop]: { ...state[action.prop], ...action.value },
+        [action.payload.prop]: {
+          ...state[action.payload.prop],
+          ...action.payload.value,
+        },
       };
     }
   } else if (action.type === 'set') {
-    if (action.key) {
+    if (action.payload?.key) {
       state = {
         ...state,
-        [action.key]: action.value,
+        [action.payload.key]: action.payload.value,
       };
     } else {
       state = {
-        ...action.value,
+        ...action.payload?.value,
       };
     }
-  } else if (action.type === 'remove' && action.key) {
-    delete state[action.key];
+  } else if (action.type === 'remove' && action.payload?.key) {
+    delete state[action.payload.key];
   } else if (action.type === 'reset') {
     state = { ...initialState };
+  } else if (typeof action === 'object' && !action.type) {
+    // assign
+    state = {
+      ...state,
+      ...action,
+    };
   }
   return state;
 };
 
 export const withContext =
-  (WrappedComponent: ComponentType | any) => (props: any) =>
-    (
-      <Context.Provider value={useReducer(reducer, initialState)}>
-        <WrappedComponent {...props} />
+  (WrappedComponent: ComponentType | any) => (props: any) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    return (
+      <Context.Provider value={state}>
+        <WrappedComponent {...props} disPatch={dispatch} />
       </Context.Provider>
     );
+  };
 
-export const useStateValue = () => useContext(Context);
 export default withContext;
 
 /*
