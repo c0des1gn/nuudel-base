ff048a4cc76ed279455d93ed6e0770af47c9ef85
Damdinsuren Dorjsuren <damdinsuren@live.com>
2022-03-11 11:31:37 +0800

    context

--- src/lib/hocs/index.ts
+++ src/lib/hocs/index.ts
@@ -1,14 +1,5 @@
 import withRedux, { initStore, store } from './withRedux';
 import withApollo from './withApollo';
-import withReducer from './withReducer';
 import withContext, { Context } from './withContext';
 
-export {
-  withRedux,
-  withApollo,
-  withContext,
-  Context,
-  initStore,
-  withReducer,
-  store,
-};
+export { withRedux, withApollo, withContext, Context, initStore, store };
--- src/lib/hocs/withContext/index.tsx
+++ src/lib/hocs/withContext/index.tsx
@@ -1,3 +1,72 @@
+import React, {
+  useReducer,
+  createContext,
+  ComponentType,
+  useContext,
+} from 'react';
+import DeviceInfo from 'react-native-device-info';
+import { ViewMode } from 'nuudel-utils';
+
+// Declaring the state object globally.
+let initialState: any = {
+  sort: '',
+  view: DeviceInfo.isTablet() ? ViewMode.CardList : ViewMode.List,
+  theme: 'light',
+};
+
+// Create a context for a user
+export const Context = createContext(initialState);
+Context.displayName = 'mainContext';
+
+const reducer = (state, action): any => {
+  if (action.type === 'assign') {
+    state = {
+      ...state,
+      ...action.value,
+    };
+  } else if (action.type === 'set' && action.prop) {
+    if (action.key) {
+      state = {
+        ...state,
+        [action.prop]: { ...state[action.prop], [action.key]: action.value },
+      };
+    } else {
+      state = {
+        ...state,
+        [action.prop]: { ...state[action.prop], ...action.value },
+      };
+    }
+  } else if (action.type === 'set') {
+    if (action.key) {
+      state = {
+        ...state,
+        [action.key]: action.value,
+      };
+    } else {
+      state = {
+        ...action.value,
+      };
+    }
+  } else if (action.type === 'remove' && action.key) {
+    delete state[action.key];
+  } else if (action.type === 'reset') {
+    state = { ...initialState };
+  }
+  return state;
+};
+
+export const withContext =
+  (WrappedComponent: ComponentType | any) => (props: any) =>
+    (
+      <Context.Provider value={useReducer(reducer, initialState)}>
+        <WrappedComponent {...props} />
+      </Context.Provider>
+    );
+
+export const useStateValue = () => useContext(Context);
+export default withContext;
+
+/*
 import React, { ComponentType, useState, useRef } from 'react';
 import DeviceInfo from 'react-native-device-info';
 import { ViewMode } from 'nuudel-utils';
@@ -36,3 +105,4 @@ const withContext = (WrappedComponent: ComponentType | any) => (props: any) => {
 };
 
 export default withContext;
+*/
